385d995cca2493bf0df5b475e45b1dcbfd0b72f4
Memseletu Moundae <memseletumoundae@Memseletus-MacBook-Air.local>
2022-09-16 14:09:41 -0400

    mem

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,17 +5,21 @@ public class App {
         //1-create a scanner
         Scanner userInput = new Scanner(System.in);
         String name;
+
+    while(true) {
         System.out.print("Enter Your Name: ");
-
         name = userInput.nextLine();
-        if (name == "Alice" && name == "Bob") {
+        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
             System.out.println("Hey " + name + "!");
+            break;
         }
 
-
         }
-
+    }
 
     }
+
+
+
 
 
